ce9da842abac7296a09a0a2b00baf35877509787
Kimihiro Ono <kimihiro.add@gmail.com>
2025-04-06 17:57:13 +0900

    トップページ作成中

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kimihiro222.github.io/',
-	base: './',
-	integrations: [mdx(), sitemap()],
+  site: 'https://kimihiro222.github.io/',
+  base: './',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
